86f40d62b715a357837d1e282c88bc72c49ce5de
Tomasz <rykala79@o2.pl>
2023-03-06 22:05:56 +0100

    4.page_createPost

--- client/src/page/CreatePost.jsx
+++ client/src/page/CreatePost.jsx
@@ -1,9 +1,80 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { preview } from "../assets";
+import { getRandomPrompt } from "../utils";
+import { FormField, Loader } from "../components";
 
 const CreatePost = () => {
-  return (
-    <div>CreatePost</div>
-  )
-}
+  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    name: "",
+    prompt: "",
+    photo: "",
+  });
+  const [generatingImg, setGeneratingImg] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-export default CreatePost
+  const handleSubmit = () => {};
+
+  const handleChange = (e) => {};
+
+  const handleSupriseMe = () => {};
+
+  return (
+    <section className="max-w-7xl mx-auto">
+      <div>
+        <h1 className="font-extrabold text-[#18277ee5] text-[32px]">Create</h1>
+        <p className="mt-2 text-[#2e308fef] text-[16px] max-w [500px]">
+          Create imaginative and visually stunning images through DALL-E AI and
+          share them with community.
+        </p>
+      </div>
+
+      <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
+        <div className="flex flex-col gap-5">
+          <FormField
+            labelName="Your Name"
+            type="text"
+            name="name"
+            placeholder="john smith"
+            value={form.name}
+            handleChange={handleChange}
+          />
+
+          <FormField
+            labelName="Prompt"
+            type="text"
+            name="prompt"
+            placeholder="A plush toy robot sitting against a yellow wall"
+            value={form.prompt}
+            handleChange={handleChange}
+            isSupriseMe
+            handleSupriseMe={handleSupriseMe}
+          />
+
+          <div
+            className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
+           focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center"
+          >
+            {form.photo ? (
+              <img
+                src={form.photo}
+                alt={form.prompt}
+                className="w-full h-full object-contain"
+              />
+            ) : (
+              <img
+                src={preview}
+                alt="preview"
+                className="w-9/12 h-9/12 object-contain opacity-40"
+              />
+            )}
+          </div>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default CreatePost;

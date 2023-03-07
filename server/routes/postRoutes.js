9270fd8435db538a0518125a4ddee2e78781f440
Tomasz <rykala79@o2.pl>
2023-03-07 21:23:01 +0100

    Backend_server

---
+++ server/routes/postRoutes.js
@@ -0,0 +1,46 @@
+import  express  from "express";
+import * as dotenv  from "dotenv";
+import { v2 as cloudinary } from "cloudinary";
+
+import Post from '../mongodb/models/post.js';
+
+dotenv.config();
+
+const router = express.Router();
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
+
+//GET ALL POSTS
+
+router.get('/', async (req, res) => {
+    try {
+        const posts = await Post.find({});
+        res.status(200).json({ sucess: true, data: posts });
+    } catch (error) {
+        res.status(500).json({ sucess: false, message: error })
+    }
+});
+
+//CREATE POST
+
+router.post('/:id', async (req, res) => {
+    try {
+        const { name, prompt, photo } = req.body;
+    const photoURL = await cloudinary.uploader.upload(photo);
+    const newPost = new Post({
+        name,
+        prompt,
+        photo: photoURL.url,
+    });
+
+    res.status(201).json({ sucess: true, data: newPost });
+    } catch (error) {
+        res.status(500).json({ sucess: false, message: error })
+    }
+});
+
+export default router;

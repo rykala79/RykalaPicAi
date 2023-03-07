4d6e2e8768a52f2b32c48744cbebcbb2517f0432
Tomasz <rykala79@o2.pl>
2023-03-07 11:37:12 +0100

    5.Complete_CreatePost

--- client/src/page/CreatePost.jsx
+++ client/src/page/CreatePost.jsx
@@ -15,11 +15,18 @@ const CreatePost = () => {
   const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
 
+  const generateImage = () => {};
+
   const handleSubmit = () => {};
 
-  const handleChange = (e) => {};
+  const handleChange = (e) => {
+    setForm({...form, [e.target.name]: e.target.value });
+  };
 
-  const handleSupriseMe = () => {};
+  const handleSupriseMe = () => {
+    const randomPrompt = getRandomPrompt(form.prompt);
+    setForm({...form, prompt: randomPrompt });
+  };
 
   return (
     <section className="max-w-7xl mx-auto">
@@ -70,7 +77,39 @@ const CreatePost = () => {
                 className="w-9/12 h-9/12 object-contain opacity-40"
               />
             )}
+            {generatingImg && (
+              <div
+                className="absolute inset-0 z-0 flex justify-center
+               items-center bg-[rgba(0,0,0,0.5)] rounded-lg"
+              >
+                <Loader />
+              </div>
+            )}
           </div>
+        </div>
+
+        <div className="mt-5 flex gap-5">
+          <button
+            type="button"
+            onClick={generateImage}
+            className="text-white bg-blue-500 font-medium rounded-md 
+           text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+          >
+            {generatingImg ? "Generating..." : "Generate"}
+          </button>
+        </div>
+
+        <div className="mt-10">
+          <p className="mt-2 text-[#2f2681a2] text-[14px]">
+            Once you have created the image , you can share it with others
+          </p>
+          <button
+            type="submit"
+            className="mt-3 text-white  bg-[#6469ff] font-medium rounded-md
+               text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+          >
+            {loading ? "Sharing..." : "Share with others"}
+          </button>
         </div>
       </form>
     </section>
